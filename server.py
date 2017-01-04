dba7e2c44ee45a988f551bbed5b444bfe24fe41e
jiehua <1753970025@qq.com>
2017-01-04 11:45:07 +0800

    fix

--- server.py
+++ server.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# @author   http://chenjiehua.me
-# @date     2016-01
+# @author   https://chenjiehua.me
+# @date     2016-01-01
 #
 
 """ falcon server api """
@@ -12,11 +12,8 @@ import torndb
 import falcon
 import ujson as json
 from wsgiref import simple_server
-from statsd import StatsClient
+from etc import config
 
-import config
-
-statsd = StatsClient(**config.STATSD)
 
 class ParamsError(Exception):
 
@@ -34,24 +31,21 @@ class JokesResource(object):
         # 数据预加载
         self.category = self.db.query("SELECT `cat`, `cat_id` FROM `category` WHERE `cat_id` <> 0")
         self.category.append({"cat": u"其他", "cat_id": 0})
-        self.cat_range = {-1:[]}
+        self.cat_range = {-1: []}
         jokes = self.db.query("SELECT `id`, `cat_id` FROM `joke`")
         for joke in jokes:
             self.cat_range.setdefault(joke['cat_id'], [])
             self.cat_range[joke['cat_id']].append(str(joke['id']))
             self.cat_range[-1].append(str(joke['id']))
 
-    @statsd.timer("request")
     def on_get(self, req, resp):
         query = req.params.get("query")
         resp.status = falcon.HTTP_200
 
         if query == "cat":
-            statsd.incr("category")
-            resp.body = json.dumps({"c":0, "d": self.category})
+            resp.body = json.dumps({"c": 0, "d": self.category})
 
         elif query == "joke":
-            statsd.incr("joke")
             joke_id = req.params.get("joke_id")
             if joke_id and joke_id.isdigit():
                 ids = [joke_id]
@@ -66,8 +60,7 @@ class JokesResource(object):
                     limit = len(self.cat_range[cat])
                 ids = random.sample(self.cat_range[cat], limit)
 
-            with statsd.timer("sql_query"):
-                jokes = self.db.query("SELECT `id`, `cat`, `cat_id`, `title`, `content` FROM `joke` WHERE `id` in (%s)" % ','.join(ids))
+            jokes = self.db.query("SELECT `id`, `cat`, `cat_id`, `title`, `content` FROM `joke` WHERE `id` in (%s)" % ','.join(ids))
 
             if len(jokes) > 0:
                 resp.body = json.dumps({"c": 0, "d": jokes})
@@ -92,7 +85,7 @@ app.add_error_handler(ParamsError, ParamsError.handle)
 
 
 if __name__ == "__main__":
-    host = config.SERVER['host']
-    port = config.SERVER['port']
-    httpd = simple_server.make_server(host, port, app)
+    print "Start server on", config.bind
+    host, port = config.bind.split(":")
+    httpd = simple_server.make_server(host, int(port), app)
     httpd.serve_forever()
